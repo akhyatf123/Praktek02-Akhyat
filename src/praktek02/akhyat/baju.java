8f65dbce6dfe61ceae51258766f19484b0b44f8e
akhyatf <akhyatf706@gmail.com>
2018-03-27 06:16:39 +0800

    membuat dan behavior

--- src/praktek02/akhyat/baju.java
+++ src/praktek02/akhyat/baju.java
@@ -2,5 +2,14 @@
 package praktek02.akhyat;
 
 public class baju {
+    String jenis;
+    String bahan;
+    
+    void cetakInfo (){
+        System.out.println("Jenis   :"+jenis);
+        System.out.println("Bahan   :"+bahan);
+        
+                
+    }
     
 }
